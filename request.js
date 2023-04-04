891f9ec55f7d2e247164690155dbc51096130bd9
uavony <2057035911@qq.com>
2023-04-04 14:34:44 +0800

    add:xhr

---
+++ request.js
@@ -0,0 +1,27 @@
+/**
+ * 编写与XMLHttpRequest有关的代码
+ */
+
+/*
+const SERVER_URL = ''
+const xhr = new XMLHttpRequest()
+
+xhr.open('GET', SERVER_URL, true)
+
+xhr.onreadystatechange = function(){
+    if(xhr.readyState == 4 ){
+        if(xhr.status == 200){
+            // 处理更新
+        }else{
+            // 处理响应错误
+        }
+    }
+}
+
+xhr.onerror = function (){
+    // 处理请求错误
+}
+*/
+
+// ==== 使用Promise对xhr进行封装 ====
+ 
